f5fce6d060176bb60773843fdfb1a9af2807f892
Le  Minh <pkminh0807@gmail.com>
2026-01-05 11:35:02 +0700

    module mongoose

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 require("dotenv").config();
-
+const database = require("./config/database");
 const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
+database.connect();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+app.use(express.static("public"));
+
 // app.get("/", (req, res) => {
 //   res.render("client/pages/home/index");
 // });
