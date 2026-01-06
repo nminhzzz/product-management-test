38b460345db31497f33862eb7ad08aa9728d4015
Le  Minh <pkminh0807@gmail.com>
2026-01-06 19:40:32 +0700

    tính nang bo loc status

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,13 +1,43 @@
 const Products = require("../../models/product.model");
 module.exports.products = async (req, res) => {
-  console.log(">>> products controller called");
-  const products = await Products.find({
+  const filterStatus = [
+    {
+      name: "tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+  const find = {
     deleted: false,
-  });
-  console.log(products);
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Products.find(find);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Trang danh sách sản phẩmmm",
     products: products,
+    filterStatus: filterStatus,
   });
 };
--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -3,12 +3,6 @@ const controller = require("../../controllers/admin/products.controller");
 
 const route = express.Router();
 
-// Log mọi request đi qua router này để dễ debug
-route.use((req, res, next) => {
-  console.log(">>> [ROUTER] /admin/products hit:", req.method, req.originalUrl);
-  next();
-});
-
 // Dùng "/" rõ ràng để đảm bảo map đúng root
 route.get("/", controller.products);
 
