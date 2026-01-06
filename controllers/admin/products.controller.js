9155a603b5691d0e38342fe09a04a463b9a0f591
Le  Minh <pkminh0807@gmail.com>
2026-01-06 15:27:27 +0700

    lay data in ra giao dien san pham

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,5 +1,13 @@
-module.exports.products = (req, res) => {
+const Products = require("../../models/product.model");
+module.exports.products = async (req, res) => {
+  console.log(">>> products controller called");
+  const products = await Products.find({
+    deleted: false,
+  });
+  console.log(products);
+
   res.render("admin/pages/products/index", {
-    pageTitle: "Trang danh sách sản phẩm",
+    pageTitle: "Trang danh sách sản phẩmmm",
+    products: products,
   });
 };
