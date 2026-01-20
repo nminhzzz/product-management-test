50ddecd915c3d06e6ae7509a3a4fb7b77563b54a
Le  Minh <pkminh0807@gmail.com>
2026-01-20 16:37:28 +0700

    them 1 san pham - uploads anh- in public

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -122,8 +122,9 @@ module.exports.createPost = async (req, res) => {
   req.body.stock = parseInt(req.body.stock);
   if (req.body.position == "") {
     req.body.position = (await Product.countDocuments()) + 1;
-    console.log(req.body.position);
   }
+  req.body.thumbnail = `/uploads/${req.file.filename}`;
+  console.log(req.file, req.body);
   const product = new Product(req.body);
   await product.save();
   res.redirect(`${prefixAdmin}/products`);
--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const controller = require("../../controllers/admin/products.controller");
+const multer = require("multer");
 
+const controller = require("../../controllers/admin/products.controller");
+const storageMulter = require("../../helpers/storageMulter");
 const route = express.Router();
+const upload = multer({ storage: storageMulter() });
 
 // Dùng "/" rõ ràng để đảm bảo map đúng root
 route.get("/", controller.products);
 route.patch("/change-status/:status/:id", controller.changeStatus);
 route.delete("/delete/:id", controller.delete);
 route.get("/create", controller.create);
-route.post("/create", controller.createPost);
+route.post("/create", upload.single("thumbnail"), controller.createPost);
 
 route.patch("/change-multi", controller.changeMulti);
 
