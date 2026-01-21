506068c776fd801fc2a58049b0a5a69828de2f58
Le  Minh <pkminh0807@gmail.com>
2026-01-21 15:39:45 +0700

    sua de deploy len

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.use(bodyParser.urlencoded());
 const port = process.env.PORT;
 database.connect();
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 //Flash
 // Flash
