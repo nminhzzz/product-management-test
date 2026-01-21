e722601d85d30a90084fb053426ae2640bac5420
Le  Minh <pkminh0807@gmail.com>
2026-01-21 17:21:41 +0700

    m

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,18 +1,23 @@
 const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    status: "active",
-    deleted: false,
-  }).sort({ position: "desc" });
+  try {
+    const products = await Product.find({
+      status: "active",
+      deleted: false,
+    }).sort({ position: "desc" });
 
-  const newProducts = products.map((item) => {
-    item.priceNew = (
-      (item.price * (100 - item.discountPercentage)) /
-      100
-    ).toFixed(0);
-    return item;
-  });
-  res.render("client/pages/products/index", { products: newProducts });
+    const newProducts = products.map((item) => {
+      item.priceNew = (
+        (item.price * (100 - item.discountPercentage)) /
+        100
+      ).toFixed(0);
+      return item;
+    });
+    res.render("client/pages/products/index", { products: newProducts });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 };
 module.exports.detail = async (req, res) => {
   try {
